f05c8117d5c38301250b8f8d1fc0342697760b14
julf71-macrotest <jul.f71@gmail.com>
2026-03-02 19:09:51 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,1 +1,11 @@
+import streamlit as st
 
+st.set_page_config(page_title="Sport App", layout="wide")
+
+st.title("Sport App perso")
+st.write("Pages: Exercices, Programmes, Séance, Historique.")
+
+st.info(
+    "Conseil: commence par créer des exercices, puis des programmes, puis lance une séance. "
+    "Tout est stocké dans Google Sheets."
+)
